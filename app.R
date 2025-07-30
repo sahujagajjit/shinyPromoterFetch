245e54d13a7693478657d4a231e26ee4856addab
sahujagajjit <sahujagajjit@gmail.com>
2025-07-30 11:20:37 +0530

    Modified a bit to have Setaria viridis and use biomart for sequence extraction

--- app.R
+++ app.R
@@ -1,5 +1,18 @@
+# ProFetShi
+# The idea of this tool is to serve as a resource which will provide user-friendly access to promoter sequences of various species (initially plant species)
+# Author: JeeT
+# Date Created: 29 July, 2025
+# Date last modified: 30 July, 2025
+
+# To Do:
+## Modify to have more dynamic codes in terms of species
+## Add and fit options for accessing species and their datasets on biomart directly from the tool
+## Identify packages or write codes to provide promoter analysis options (may be similar to plantcare)
+## Check with possibility of parallelization to make some processes a bit faster, for example, downloading all sequences using biomart
+## Check if there is any scope for accomodating the species for which neither of BSgenome and biomart is available
+
 # Define CRAN and Bioconductor packages
-cranPackages <- c("shiny", "DT")
+cranPackages <- c("shiny", "DT", "httr")
 biocPackages <- c(
   "Biostrings", "GenomicRanges", "BSgenome", 
   "BSgenome.Athaliana.TAIR.TAIR9", "BSgenome.Osativa.MSU.MSU7", 
@@ -38,6 +51,9 @@ library(BSgenome.Osativa.MSU.MSU7)
 library(TxDb.Athaliana.BioMart.plantsmart28)
 library(biomaRt)
 
+# Develop dynamic codes for biomart use for those species for which BSgenome is not available
+## The first part i.e. Gene can be dynamic for any species for which biomart is available and if no BSgenome then in the next phase while using
+### getPromoters4mUsemart function they can be handled and the Dynamic genome part would be saved to the Gene
 
 # === Arabidopsis gene ranges ===
 ensembl_ath <- useMart("plants_mart", dataset = "athaliana_eg_gene", host = "https://plants.ensembl.org")
@@ -67,12 +83,29 @@ osativaGenes <- GRanges(
 )
 osativaGenome <- BSgenome.Osativa.MSU.MSU7
 
+# === Setaria viridis gene ranges ===
+ensembl_sviridis <- useMart("plants_mart", dataset = "sviridis_eg_gene", host = "https://plants.ensembl.org")
+
+genesBM_sviridis <- getBM(
+  attributes = c("ensembl_gene_id", "chromosome_name", "start_position", "end_position", "strand"),
+  mart = ensembl_sviridis
+)
+
+sviridisGenes <- GRanges(
+  seqnames = genesBM_sviridis$chromosome_name,
+  ranges = IRanges(start = genesBM_sviridis$start_position, end = genesBM_sviridis$end_position),
+  strand = ifelse(genesBM_sviridis$strand == 1, "+", "-"),
+  gene_id = genesBM_sviridis$ensembl_gene_id
+)
+
+
+
 # === UI ===
 ui <- fluidPage(
-  titlePanel("shinyPromoterFetch"),
+  titlePanel("shinyPromoterFetch"), # It can be renamed as Promoter Fetching Shiny; in shorts ProFetShi
   sidebarLayout(
     sidebarPanel(
-      selectInput("organism", "Choose Organism:", choices = c("Arabidopsis thaliana", "Oryza sativa", "Cicer arietinum")),
+      selectInput("organism", "Choose Organism:", choices = c("Arabidopsis thaliana", "Oryza sativa", "Setaria viridis", "Cicer arietinum")),
       numericInput("promoterLength", "Upstream Length (bp):", value = 1000, min = 100, max = 3000, step = 100),
       numericInput("downstreamLength", "Downstream Length (bp):", value = 0, min = 0, max = 3000, step = 100),
       checkboxInput("namesPromoterSeqs", "SeqIdAddons", value = FALSE),
@@ -102,6 +135,8 @@ server <- function(input, output, session) {
       athalianaGenes
     } else if (input$organism == "Oryza sativa") {
       osativaGenes
+    } else if (input$organism == "Setaria viridis") {
+      sviridisGenes
     } else {
       NULL
     }
@@ -113,6 +148,8 @@ server <- function(input, output, session) {
       athalianaGenome
     } else if (input$organism == "Oryza sativa") {
       osativaGenome
+    } else if (input$organism == "Setaria viridis") {
+      sviridisGenes
     } else {
       NULL
     }
@@ -190,6 +227,57 @@ server <- function(input, output, session) {
     return(promoterSeqs)
   }
   
+  getPromoters4mUsemart <- function(gr, upstream = 1000, downstream = 0) {
+    mart <- useMart("plants_mart", dataset = "sviridis_eg_gene",
+                    host = "https://plants.ensembl.org")
+    
+    coords <- getBM(attributes = c("ensembl_gene_id", "chromosome_name", "strand", 
+                                   "start_position", "end_position"),
+                    filters = "ensembl_gene_id",
+                    values = gr$gene_id,
+                    mart = mart)
+    
+    promoterSeqs <- DNAStringSet()
+    
+    for (i in seq_len(nrow(coords))) {
+      gene <- coords[i, ]
+      if (gene$strand == 1) {
+        start <- max(1, gene$start_position - upstream)
+        end <- gene$start_position + downstream - 1
+      } else {
+        start <- gene$end_position - downstream + 1
+        end <- gene$end_position + upstream
+      }
+      
+      url <- paste0("https://rest.ensembl.org/sequence/region/Setaria_viridis/",
+                    gene$chromosome_name, ":", start, "..", end,
+                    ifelse(gene$strand == 1, ":", ":-1"),
+                    "?content-type=text/plain")
+      
+      res <- httr::GET(url)
+      if (httr::status_code(res) == 200) {
+        seqText <- httr::content(res, "text")
+        newSeq <- DNAStringSet(seqText)
+        
+        if (input$namesPromoterSeqs) {
+          names(newSeq) <- paste0(
+            gene$ensembl_gene_id, "|", gene$chromosome_name, ":", start, "-", end,
+            "|strand:", gene$strand
+          )
+        } else {
+          names(newSeq) <- gene$ensembl_gene_id
+        }
+        
+        promoterSeqs <- append(promoterSeqs, newSeq)
+      } else {
+        warning(paste("Failed for gene:", gene$ensembl_gene_id))
+      }
+    }
+    
+    return(promoterSeqs)
+  }
+  
+  
   # Handle promoter extraction
   observeEvent(input$submit, {
     output$downloadUI <- renderUI(NULL)
@@ -217,13 +305,24 @@ server <- function(input, output, session) {
       }
       
       incProgress(0.5, detail = "Fetching sequences...")
-      seqs <- getPromoters(
-        gr = geneGR,
-        upstream = input$promoterLength,
-        downstream = input$downstreamLength,
-        genome = genome,
-        organism = input$organism
-      )
+      # Here need to modify to have an option for species for which BSgenome libraries are available if not the sequence can be downloaded using biomart itself
+      if (input$organism %in% c("Arabidopsis thaliana", "Oryza sativa")){
+        seqs <- getPromoters(
+          gr = geneGR,
+          upstream = input$promoterLength,
+          downstream = input$downstreamLength,
+          genome = genome,
+          organism = input$organism
+        )
+      } else if (input$organism == "Setaria viridis"){
+        seqs <- getPromoters4mUsemart(
+          gr = geneGR,
+          upstream = input$promoterLength,
+          downstream = input$downstreamLength
+          # organism = input$organism
+        )
+      }
+      
       
       incProgress(0.3, detail = "Done.")
       sequenceData(seqs)
